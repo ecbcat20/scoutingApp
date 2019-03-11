1df12dd9d4ab33a2d226d144d5309725784058a3
Abhayprad Jha <abhaypradjha@gmail.com>
2019-03-11 18:27:36 -0400

    End of Code Day 3, potentially fixed all errors in RadioButton.js

--- components/RadioButton.js
+++ components/RadioButton.js
@@ -5,49 +5,40 @@ class RadioButton extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            buttonColor: 'white',
             selected: false
         }
     }
 
-    changeColor = function() {
-        if (this.state.buttonColor == 'black') {
-            this.setState({
-                buttonColor: 'white',
-                selected: false
-            });
-        } else {
-            this.setState({
-                buttonColor: 'black',
-                selected: true
-            });
-        }
+    toggleSelected = function() {
+        this.setState({
+            selected: !this.state.selected
+        });
     }
     render() {
         
         return (
-            <Button title="" onPress={this.changeColor()}>
-            <View style={[{
-                height: 24,
-                width: 24,
-                borderRadius: 12,
-                borderWidth: 2,
-                borderColor: '#000',
-                alignItems: 'center',
-                justifyContent: 'center',
-            }, this.props.style]}>
-            {
-                this.props.selected ?
-                <View style={{
-                    height: 12,
-                    width: 12,
-                    borderRadius: 6,
-                    backgroundColor: this.state.buttonColor,
-                }} />
-                : null
-            }
-            </View>
-            </Button>
+            <Button title="" onPress={this.toggleSelected()}>
+                    <View style={[{
+                        height: 24,
+                        width: 24,
+                        borderRadius: 12,
+                        borderWidth: 2,
+                        borderColor: '#000',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }, this.props.style]}>
+                    {
+                        this.state.selected ?
+                        <View style={{
+                            height: 12,
+                            width: 12,
+                            borderRadius: 6,
+                            backgroundColor: 'black',
+                        }} />
+                        : null
+                    }
+                    </View>
+                </Button>
             );
         }
     }
