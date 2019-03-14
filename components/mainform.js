fd8bb74bdcdd275e0cd81b5d1b683003b01e7011
System Administrator <root@Evans-MBP.home>
2019-03-14 16:23:09 -0400

    finished form

--- components/mainform.js
+++ components/mainform.js
@@ -5,7 +5,7 @@ import t from 'tcomb-form-native'; // 0.6.9
 
 const Form = t.form.Form;
 
-const User = t.struct({
+const Scout = t.struct({
   email: t.String,
   username: t.String,
   password: t.String,
@@ -17,8 +17,10 @@ const Preliminaries = t.struct({
   'Match Type': t.String,
   'Team Number': t.String,
 })
-
+//Data collected in sandstorm 
 const SandStorm = t.struct({
+  'Descend from Level 2': t.String,
+  'Descend from Level 1': t.String,
   'Cargo in Ship - Accurate': t.Boolean,
   'Hatches on Ship - Accurate': t.Boolean,
   'Cargo in Rocket': t.Boolean,
@@ -26,6 +28,27 @@ const SandStorm = t.struct({
   'Hatches on Rocket - Low (1)': t.Boolean,
   'Confirm': t.Boolean
 
+})
+//Rocket-related fields
+const TeleOpRocket = t.struct({
+  'Rocket Hatch Level 1': t.String,
+  'Rocket Hatch Level 2': t.String,
+  'Rocket Hatch Level 3': t.String,
+  'Rocket Cargo Level 1': t.String,
+  'Rocket Cargo Level 2': t.String,
+  'Rocket Cargo Level 3': t.String,
+})
+
+const TeleOpCargoShip = t.struct({
+  'Cargo Hatches': t.String,
+  'Cargo Cargo': t.String,
+})
+
+const EndGame = t.struct({
+  'Climb Tier 3': t.String,
+  'Climb Tier 3': t.String,
+  'Park': t.String,
+  'None': t.String,
 })
 
 export default class App extends Component {
@@ -55,9 +78,9 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Form type={User} />
+        <Form type={Scout} />
         <Button
-          title="Sign Up!"
+          title="Submit"
           onPress={this.handleSubmit}
         />
       </View>
