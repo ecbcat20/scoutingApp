a92f73384f351c1332bc0a22af5ea5343a811421
Evan Belsky <ecbcat20@gmail.com>
2019-03-14 13:33:19 -0400

    Began working on real form

--- components/mainform.js
+++ components/mainform.js
@@ -1,32 +1,66 @@
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
 
-class NameForm extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {value: 'Team Number'};
+import t from 'tcomb-form-native'; // 0.6.9
 
-  
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
-  
-    handleChange(event) {
-      this.setState({value: event.target.value});
-    }
-  
-    handleSubmit(event) {
-      alert('A name was submitted: ' + this.state.value);
-      event.preventDefault();
-    }
-  
-    render() {
-      return (
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Name:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      );
-    }
+const Form = t.form.Form;
+
+const User = t.struct({
+  email: t.String,
+  username: t.String,
+  password: t.String,
+  terms: t.Boolean
+
+  })
+
+const Preliminaries = t.struct({
+  'Match Type': t.String,
+  'Team Number': t.String,
+})
+
+const SandStorm = t.struct({
+  'Cargo in Ship - Accurate': t.Boolean,
+  'Hatches on Ship - Accurate': t.Boolean,
+  'Cargo in Rocket': t.Boolean,
+  'Hatches on Rocket - High (2,3)': t.Boolean, default:"false",
+  'Hatches on Rocket - Low (1)': t.Boolean,
+  'Confirm': t.Boolean
+
+})
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Form type={User} /> {/* Notice the addition of the Form component */}
+      </View>
+    );
   }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    marginTop: 50,
+    padding: 20,
+    backgroundColor: '#ffffff',
+  },
+});
+
+export default class App extends Component {
+  handleSubmit = () => {
+    // do the things  
+  }
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <Form type={User} />
+        <Button
+          title="Sign Up!"
+          onPress={this.handleSubmit}
+        />
+      </View>
+    );
+  }
+}
